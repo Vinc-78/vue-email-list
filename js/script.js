4cbb62b7f6d40e12f3f1a1f4ea4063bc6c16c781
Vinc-78 <vincenzodicostanzo.um@gmail.com>
2021-11-09 15:11:31 +0100

    genera la lista da API

---
+++ js/script.js
@@ -0,0 +1,47 @@
+Vue.config.devtools = true;
+
+new Vue({
+  el: "#root",
+  data: {
+    urlAPI: "https://flynn.boolean.careers/exercises/api/random/mail",
+    arrayEmail: [],
+    
+    pandingCall: 0,
+    
+
+
+  },
+
+  methods: {
+
+    generaEmail() {
+      
+      this.arrayEmail = [];
+
+      
+      for (let i = 0; i < 10; i++) {
+
+        axios.get(this.urlAPI).then((ajaxResponse) => {
+
+          const rispostaServer = ajaxResponse.data;
+
+          this.arrayEmail.push(rispostaServer.response);
+
+        })
+
+        
+      }
+
+      console.log(this.arrayEmail)
+
+    },
+
+    
+
+  }
+
+});
+
+
+
+
